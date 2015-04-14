3431a5419f04f3e8bf4182c50573248047a55d90
chrisyao4700 <chrisyao900908@gmail.com>
2015-04-14 17:55:45 -0400

    Add Test Case

---
+++ src/com/team/test/YuNameTestCase.java
@@ -0,0 +1,26 @@
+package com.team.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.yu.implement.LYNameCreater;
+
+public class YuNameTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals("Yu Luo", LYNameCreater.getName());
+	}
+
+}
